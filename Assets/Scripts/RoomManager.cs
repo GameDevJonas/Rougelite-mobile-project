1fa874baadc07a64697b30fa1b6bd7f636f4dca4
Jonas <paandab321@gmail.com>
2020-03-02 16:50:55 +0100

    Tilemap shiz, not important

---
+++ Assets/Scripts/RoomManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomManager : MonoBehaviour
+{
+    public GameObject[] topRooms;
+    public GameObject[] bottomRooms;
+    public GameObject[] rightRooms;
+    public GameObject[] leftRooms;
+
+    public GameObject[] closedRooms;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
