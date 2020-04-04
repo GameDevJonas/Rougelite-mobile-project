41ea721b8511e010651f99376fca6807cdc57071
Egil <vumsy@hotmail.com>
2020-04-04 17:12:19 +0200

    Stats, Modifiers, Inventory, Loot og ItemDatabase
    CharacterStats og StatModifiers brukes for å håndtere stats, Items, ItemDatabase og Inventory brukes for å håndtere items. Laget CommonLootDrop object som gir tilfeldig item.

---
+++ Assets/Scripts/RueStats.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RueStats : MonoBehaviour
+{
+    public CharacterStat Health;
+    public CharacterStat Strength;
+    public CharacterStat StrengthPercent;
+    public CharacterStat Dexterity;
+    public CharacterStat CritChance;
+    public CharacterStat CritDamage;
+    public CharacterStat LifeOnHit;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
