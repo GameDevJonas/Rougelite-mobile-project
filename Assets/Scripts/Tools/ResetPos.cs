e44c88c729f57d536c4edde96be39dd9625ba128
Jonas <paandab321@gmail.com>
2020-04-05 02:47:56 +0200

    Begynt på nytt dungeon system

---
+++ Assets/Scripts/Tools/ResetPos.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class ResetPos : MonoBehaviour
+{
+    [MenuItem("Cool Tools/Shortcuts/Reset Transform #s")]
+    public static void ResetTransform()
+    {
+        var selectedObject = Selection.activeGameObject;
+
+        Undo.RecordObject(selectedObject.transform, "Reset Transform on " + selectedObject.name);
+
+        if (!selectedObject)
+        {
+            return;
+        }
+
+        selectedObject.transform.position = Vector3.zero;
+
+        EditorUtility.SetDirty(selectedObject.transform);
+    }
+}
