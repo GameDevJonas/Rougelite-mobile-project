c6f515b26713fbdd6eb6a366cc3dfc127f3e6059
Jonas <paandab321@gmail.com>
2020-04-28 21:20:28 +0200

    Dungeon movement!

---
+++ Assets/Imported DungeonGen/Scripts/DebuggingInBuild.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+using Cinemachine;
+
+public class DebuggingInBuild : MonoBehaviour
+{
+    public Slider slider;
+
+    public TextMeshProUGUI sliderText;
+
+    public CinemachineVirtualCamera vCam;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        sliderText.text = "Value: " + slider.value;
+        vCam.m_Lens.OrthographicSize = slider.value;
+    }
+}
---
+++ Assets/Imported DungeonGen/Scripts/Door.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+    public Transform upStartDir, leftStartDir, rightStartDir, downStartDir;
+
+    public float length;
+
+    public LayerMask whatLayer;
+    void Start()
+    {
+        length = 6f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit2D upRay = Physics2D.Raycast(upStartDir.position, Vector2.right, length, whatLayer);
+        RaycastHit2D leftRay = Physics2D.Raycast(leftStartDir.position, Vector2.up, length, whatLayer);
+        RaycastHit2D rightRay = Physics2D.Raycast(rightStartDir.position, Vector2.down, length, whatLayer);
+        RaycastHit2D downRay = Physics2D.Raycast(downStartDir.position, Vector2.left, length, whatLayer);
+
+        if (upRay)
+        {
+            Debug.Log(upRay.collider.gameObject + " upRay", gameObject);
+            upRay.collider.gameObject.transform.position += new Vector3(0, 110, 0);
+        }
+        else if (leftRay)
+        {
+            Debug.Log(leftRay.collider.gameObject + " leftRay", gameObject);
+            leftRay.collider.gameObject.transform.position -= new Vector3(190, 0, 0);
+        }
+        else if (rightRay)
+        {
+            Debug.Log(rightRay.collider.gameObject + " rightRay", gameObject);
+            rightRay.collider.gameObject.transform.position += new Vector3(190, 0, 0);
+        }
+        else if (downRay)
+        {
+            Debug.Log(downRay.collider.gameObject + " downRay", gameObject);
+            downRay.collider.gameObject.transform.position -= new Vector3(0, 110, 0);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Debug.DrawRay(upStartDir.position, Vector2.right * length, Color.red);
+        Debug.DrawRay(leftStartDir.position, Vector2.up * length, Color.blue);
+        Debug.DrawRay(rightStartDir.position, Vector2.down * length, Color.black);
+        Debug.DrawRay(downStartDir.position, Vector2.left * length, Color.green);
+    }
+}
