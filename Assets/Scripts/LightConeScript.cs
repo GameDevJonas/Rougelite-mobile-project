6d02d7b9a9c44e550b8558b528054bd4b08b6dbb
Egil <vumsy@hotmail.com>
2020-06-05 22:49:54 +0200

    Light tweaking and Has eye debuff affects light

--- Assets/Scripts/LightConeScript.cs
+++ Assets/Scripts/LightConeScript.cs
@@ -1,15 +1,23 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Experimental.Rendering.Universal;
 
 public class LightConeScript : MonoBehaviour
 {
     public string direction;
 
+    public Light2D light2D; 
 
     // Update is called once per frame
     public void RotateMeBaby(string dir, float HasEye)
     {
+        if (HasEye > 0)
+        {
+            light2D = gameObject.GetComponent<Light2D>();
+            light2D.pointLightOuterAngle = 160;
+            light2D.pointLightInnerRadius = 15;
+        }
         if (dir == "UL" && HasEye <= 0)
         {
             direction = dir;
@@ -53,42 +61,42 @@ public class LightConeScript : MonoBehaviour
         else if (dir == "UL" && HasEye > 0)
         {
             direction = dir;
-            gameObject.transform.rotation = Quaternion.Euler(0, 0, 45);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 60);
         }
         else if (dir == "U" && HasEye > 0)
         {
             direction = dir;
-            transform.Rotate(0, 0, 15);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 15);
         }
         else if (dir == "UR" && HasEye > 0)
         {
             direction = dir;
-            transform.Rotate(0, 0, -20);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, -60);
         }
         else if (dir == "R" && HasEye > 0)
         {
             direction = dir;
-            transform.Rotate(0, 0, -75);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, -75);
         }
         else if (dir == "DR" && HasEye > 0)
         {
             direction = dir;
-            transform.Rotate(0, 0, -120);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, -150);
         }
         else if (dir == "D" && HasEye > 0)
         {
             direction = dir;
-            transform.Rotate(0, 0, -165);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 195);
         }
         else if (dir == "DL" && HasEye > 0)
         {
             direction = dir;
-            transform.Rotate(0, 0, 150);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 150);
         }
         else if (dir == "L" && HasEye > 0)
         {
             direction = dir;
-            transform.Rotate(0, 0, 105);
+            gameObject.transform.rotation = Quaternion.Euler(0, 0, 105);
         }
     }
 }
